b9732717027b1744eec785038ab013edefabda9c
SOHYEON <44013936+sohyunwriter@users.noreply.github.com>
2020-01-20 21:27:02 +0900

    Update 1806(부분합).cpp

--- 백준/1806(부분합).cpp
+++ 백준/1806(부분합).cpp
@@ -1,3 +1,10 @@
+/*
+Name : Sohyeon Yim
+Email : brightcattle@gmail.com 
+Blog : https://sohyunwriter.tistory.com/
+Problem : https://www.acmicpc.net/problem/1806
+*/
+
 #include <iostream>
 #include <string>
 #include <vector>
