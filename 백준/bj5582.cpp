e8348848023aa9f636cbe31ccc28742545396654
SOHYEON <44013936+sohyunwriter@users.noreply.github.com>
2020-03-05 01:12:26 +0900

    Create bj5582.cpp

---
+++ 백준/bj5582.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define MAX 4001
+
+int main() {
+	string s1, s2;
+	int dp[MAX][MAX];
+	int ans = 0; // 빈 문자열
+
+	cin >> s1;
+	cin >> s2;
+
+	for (int i = 0; i < s1.length(); i++) {
+		for (int j = 0; j < s2.length(); j++) {
+			if (s1.at(i) != s2.at(j)) {
+				dp[i][j] = 0;
+			}
+			else {
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+				if (dp[i][j] > ans)
+					ans = dp[i][j];
+			}
+
+		}
+	}
+
+	cout << ans;
+
+	return 0;
+}
