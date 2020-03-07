0e684adc8d1d7d07039c6fb03ed5012b6241f4d3
SOHYEON <44013936+sohyunwriter@users.noreply.github.com>
2020-03-07 23:37:02 +0900

    Create 155(Min Stack).cpp
    https://sohyunwriter.tistory.com/75

---
+++ LeetCode/155(Min Stack).cpp
@@ -0,0 +1,48 @@
+#include <stack>
+using namespace std;
+
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    stack<int> s;
+    stack<int> minS;
+    
+    MinStack() {
+        
+    }
+    
+    void push(int x) {
+        s.push(x);
+        if(minS.empty())
+            minS.push(x);
+        else{
+            int pastMin = minS.top();
+            if(x < pastMin)
+                minS.push(x);
+            else
+                minS.push(pastMin);
+        }
+    }
+    
+    void pop() {
+        s.pop();
+        minS.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return minS.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
