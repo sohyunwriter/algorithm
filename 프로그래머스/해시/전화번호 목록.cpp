a4cef096ac89eb61c3b3d3ec75a80f59dffcc47c
SOHYEON <44013936+sohyunwriter@users.noreply.github.com>
2020-04-09 02:16:49 +0900

    Create 전화번호 목록.cpp

---
+++ 프로그래머스/해시/전화번호 목록.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    unordered_map<string, int> map;
+    
+    for(auto i : phone_book)
+        ++map[i];
+    
+    for(int i = 0; i < phone_book.size(); i++){
+        string number = "";
+        for(int j = 0; j < phone_book[i].size(); j++){
+            number += phone_book[i][j];
+            if(map[number] > 0 && phone_book[i] != number)
+                return false;
+        }
+    }
+        
+        
+    return true;
+}
