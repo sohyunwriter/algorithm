c8464e97c6289de22dcb3b56d4357caafea5cc43
SOHYEON <44013936+sohyunwriter@users.noreply.github.com>
2020-04-11 04:11:05 +0900

    Create K번째수.cpp

---
+++ 프로그래머스/정렬/K번째수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    vector<int> a;
+    
+    for(int i = 0; i < commands.size(); i++){
+        a = array;
+        sort(a.begin() + commands[i][0]-1, a.begin()+commands[i][1]);
+        answer.push_back(a[commands[i][0] + commands[i][2] -2]);
+    }
+    return answer;
+}
