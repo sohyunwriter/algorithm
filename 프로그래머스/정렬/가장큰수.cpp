02bf43c3663aa16d69e4328225c0d1abcfea81d9
SOHYEON <44013936+sohyunwriter@users.noreply.github.com>
2020-04-14 16:18:17 +0900

    Create 가장큰수.cpp

---
+++ 프로그래머스/정렬/가장큰수.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(string a, string b){
+    return a + b > b + a;
+}
+
+string solution(vector<int> numbers) {
+    string answer = "";
+    vector<string> v;
+    
+    for(auto i:numbers){
+        v.push_back(to_string(i));
+    }
+    sort(v.begin(), v.end(), compare);
+    if(v[0] == "0"){ // {0, 0, 0, 0} -> "0"
+        return "0";
+    }
+    for(auto i : v)
+        answer += i;
+    return answer;
+}
