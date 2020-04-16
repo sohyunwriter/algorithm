bf7fc20c263f84d87996ab40da69eea99ab73af9
SOHYEON <44013936+sohyunwriter@users.noreply.github.com>
2020-04-17 02:23:40 +0900

    Create upperbound.cpp

---
+++ basic/Two Pointers/upperbound.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int myupperbound(vector<int> v, int target) { //upperbound
+	sort(v.begin(), v.end());
+
+	int start = 0;
+	int end = v.size();
+	int mid;
+
+	while (start < end) { // start == end가 되는 시점 종료
+		mid = (start + end) / 2;
+
+		if (v[mid] > target)
+			end = mid;
+		else
+			start = mid + 1;
+	}
+	return end;
+}
+
+
+
+int main() { /* this is for test */
+	vector<int> v;
+	int n, target, input;
+	scanf("%d %d", &n, &target);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &input);
+		v.push_back(input);
+	}
+	sort(v.begin(), v.end());
+  
+	int ans2 = myupperbound(v, target);
+	iter = upper_bound(v.begin(), v.end(), target);
+	int reans2 = iter - v.begin();
+	cout << ans2 << " " << reans2 << endl;
+
+	return 0;
+}
