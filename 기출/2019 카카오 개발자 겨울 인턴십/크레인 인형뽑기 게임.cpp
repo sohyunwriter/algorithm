8a586ae243fa5e22989ac44aa8d746ce75e43220
SOHYEON <brightcattle@gmail.com>
2020-05-08 12:03:05 +0900

    Create 크레인 인형뽑기 게임.cpp

---
+++ 기출/2019 카카오 개발자 겨울 인턴십/크레인 인형뽑기 게임.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int solution(vector<vector<int>> board, vector<int> moves) {
+    int answer = 0;
+    stack <int> s;
+    for(int i = 0; i < moves.size(); i++){
+        for(int j = 0; j < board.size(); j++){
+            if(board[j][moves[i]-1] != 0){
+                if(!s.empty() && s.top() == board[j][moves[i]-1]){
+                    s.pop();
+                    answer += 2;
+                }
+                else{
+                    s.push(board[j][moves[i]-1]);
+                }
+                board[j][moves[i]-1] = 0;
+                break;
+            }
+        }
+    }
+    
+    return answer;
+}
