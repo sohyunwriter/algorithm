b4e88ff69ac32b6a7bd05edb770d62720168ae0f
SOHYEON <brightcattle@gmail.com>
2020-11-13 12:26:54 +0900

    Update 332. Reconstruct Itinerary.py

--- LeetCode/332. Reconstruct Itinerary.py
+++ LeetCode/332. Reconstruct Itinerary.py
@@ -4,12 +4,12 @@ class Solution(object):
         graph = defaultdict(list)
         route = []
 
-        for departure, arrival in sorted(tickets): 
+        for departure, arrival in sorted(tickets): # 처음에 정렬하고 넣기
             graph[departure].append(arrival)
 
         def dfs(a):
             while graph[a]:
-                dfs(graph[a].pop(0))
+                dfs(graph[a].pop(0))  # 맨 처음 값 계속 pop하는 식으로 구현(재방문x)
             route.append(a)
 
         dfs("JFK")
