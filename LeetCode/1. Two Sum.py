8d9df24a731eea7c6887054d074af70bd442b723
SOHYEON <brightcattle@gmail.com>
2020-12-19 16:41:33 +0900

    Create 1. Two Sum.py

---
+++ LeetCode/1. Two Sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        # 브루트 포스로 계산 O(n^2)
+        for i in range(0, len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
