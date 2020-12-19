2acf4a2e30295d8321cfad956c156e26169f7f8e
SOHYEON <brightcattle@gmail.com>
2020-12-19 16:43:18 +0900

    Create 1. Two Sum (sol4).py

---
+++ LeetCode/1. Two Sum (sol4).py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        nums_map = {}
+        # 하나의 for 문으로 통합
+        for i, num in enumerate(nums):
+            if target - num in nums_map:
+                return [nums_map[target-num], i]
+            nums_map[num] = i
