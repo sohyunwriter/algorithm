55db0a1b91fed2cc1c7fa84ddd6689417f831b35
SOHYEON <brightcattle@gmail.com>
2021-01-02 19:04:04 +0900

    Update NumberOfDiscIntersections.py

--- codility/NumberOfDiscIntersections.py
+++ codility/NumberOfDiscIntersections.py
@@ -1,23 +1,23 @@
 def solution(A):
     temp = []
-    ans = 0
-    count = 0
+    ans, count = 0, 0
+
     if not A or len(A) == 1:
         return 0
 
     for i, v in enumerate(A):
-        temp.append((i-v, i, 0))
-        temp.append((i+v, i, 1))
+        temp.append((i - v, 0))
+        temp.append((i + v, 1))
 
-    temp.sort(key=lambda x: (x[0], x[2]))
-    
-    for p1, p2, p3 in temp:
-        if not p3:
+    temp.sort(key=lambda x: (x[0], x[1]))
+
+    for _, flag in temp:
+        if not flag:
             ans += count
             count += 1
         else:
             count -= 1
-        
+
         if ans > 10000000:
             return -1
 
