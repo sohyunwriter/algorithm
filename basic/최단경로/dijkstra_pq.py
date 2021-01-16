452105062f29cdaa699cd15fe4dca9a68007d719
SOHYEON <brightcattle@gmail.com>
2021-01-16 14:41:03 +0900

    Update dijkstra_pq.py

--- basic/최단경로/dijkstra_pq.py
+++ basic/최단경로/dijkstra_pq.py
@@ -1,49 +1,64 @@
-import heapq
+'''
+# dijkstra algorithm without priorityQueue
+# time complexity : O(V^2)
+# space complexity : O(V+E)
+'''
+
 import sys
 input = sys.stdin.readline
-
 INF = int(1e9)  # 무한을 의미하는 값으로 10억을 설정
 
-def dijkstra(graph, pos, distance, visited, n):
-    q = []
+# 방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호를 반환 : O(V)
+def get_smallest_node(distance, visited):
+    min_value = INF
+    index = 0  # 가장 최단 거리가 짧은 노드(인덱스)
+    for i in range(1, n+1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index = i
+    return index
 
-    # 시작노드로 가기 위한 최단 경로는 0으로 설정해, 큐에 삽입
-    heapq.heappush(q, (0, pos))
+# 다익스트라 알고리즘 : O(V^2)
+def dijkstra(graph, pos, n):
+    # 방문한 적이 있는지 체크하는 목적의 리스트 # sc: O(V)
+    visited = [False] * (n + 1)
+
+    # 최단 거리 테이블을 모두 무한으로 초기화 # sc: O(V)
+    distance = [INF] * (n + 1)
+
+    # 시작 노드에 대해서 초기화
     distance[pos] = 0
+    visited[pos] = True
+    for j in graph[pos]:
+        distance[j[0]] = j[1]
 
-    while q: # 큐가 비어있지 않다면
-        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
-        dist, now = heapq.heappop(q)
-
-        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
-        if distance[now] < dist:
-            continue
-
-        # 현재 노드와 연결된 다른 인접한 노드들 확인
-        for i in graph[now]:
-            cost = distance[now] + i[1]
+    # 시작 노드를 제외한 전체 n - 1 개의 노드에 대해 반복 : O(V^2)
+    for i in range(n-1):  # tc: O(V)
+        # 현재 최단 거리가 가장 짧은 노드를 꺼내서, 방문 처리
+        now = get_smallest_node(distance, visited) # tc: O(V)
+        visited[now] = True
+        # 현재 노드와 연결된 다른 노드 확인 # O(V) (node-node 간 중복된 edge가 있으면 O(E))
+        for j in graph[now]:
+            cost = distance[now] + j[1]
             # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
 
     return distance
 
-def solve(n, m, pos, edges):
-    # 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트
-    graph = [[] for i in range(n + 1)]
-    # 방문한 적이 있는지 체크하는 목적의 리스트
-    visited = [False] * (n + 1)
-    # 최단 거리 테이블을 모두 무한으로 초기화
-    distance = [INF] * (n + 1)
-
-    # edges -> graph 만들기
+# edges -> graph 만들기
+def edges2graph(edges, n):
+    graph = [[] for i in range(n+1)]  # sc: O(V)
     for start, dest, cost in edges:
         graph[start].append((dest, cost))
+    return graph
 
-    dijkstra(graph, pos, distance, visited, n)
-
-    # 모든 노드로 가기 위한 최단 거리
+def solve(n, m, pos, edges):
+    # edges -> graph
+    graph = edges2graph(edges, n)
+    # dijkstra
+    distance = dijkstra(graph, pos, n)  # 그래프, 시작위치,노드 개수
+    # 모든 노드로 가기 위한 최단 거리 반환
     return distance
 
 if __name__ == "__main__":
@@ -51,8 +66,9 @@ if __name__ == "__main__":
     n, m = map(int, input().split())
     # 시작 노드 번호를 입력받기
     pos = int(input())
-    edges = [list(map(int, input().split())) for _ in range(m)]
-
+    # 간선 정보 입력받기
+    edges = [list(map(int, input().split())) for _ in range(m)]  # sc: O(E)
+    
     distance = solve(n, m, pos, edges)
 
     # 모든 노드로 가기 위한 최단 거리 출력
