0d09557668d162fab95784da3cc2e2b7268899de
SOHYEON <brightcattle@gmail.com>
2021-02-05 02:43:44 +0900

    Create 2748(피보나치 수 2).py

---
+++ 백준/2748(피보나치 수 2).py
@@ -0,0 +1,20 @@
+# recursion with memoization
+# time complexity : O(N)
+
+def solve(n: int) -> int:
+    dp = [0, 1] + [-1] * (n+1)  # 0, 1 값 저장(종료조건) 및 -1로 초기화(not visited)
+
+    def fibo(n: int) -> int:
+        # 이미 방문한 노드인 경우
+        if dp[n] != -1:
+            return dp[n]
+
+        # memoization하며 방문
+        dp[n] = fibo(n-1) + fibo(n-2)
+        return dp[n]
+
+    ans = fibo(n)
+    return ans
+
+n = int(input())
+print(solve(n))
