e27fd880b113714bf7d221f4b1e685e894cb8245
SOHYEON <brightcattle@gmail.com>
2021-02-09 14:47:50 +0900

    Update 14501(퇴사).py

--- 백준/14501(퇴사).py
+++ 백준/14501(퇴사).py
@@ -1,17 +1,18 @@
 def solve(N, t, p):
-	dp = [0]*(N+2)
-	for i in range(1, N+1):
-		if (i+t[i]) <= N+1:
-			dp[i+t[i]] = max(dp[i]+p[i], dp[i+t[i]])
-		dp[i+1] = max(dp[i], dp[i+1])
-	return max(dp)
+    dp = [0] * (N + 2)
+    for i in range(1, N + 1):
+        if (i + t[i]) <= N + 1:
+            dp[i + t[i]] = max(dp[i] + p[i], dp[i + t[i]])
+        dp[i + 1] = max(dp[i], dp[i + 1])
+    return max(dp)
+
 
 N = int(input())
-t = [0] * (N+1)
-p = [0] * (N+1)
+t = [0] * (N + 1)
+p = [0] * (N + 1)
 
-for i in range(1, N+1):
-	ti, pi = map(int, (input().split()))
-	t[i], p[i] = ti, pi
-	
+for i in range(1, N + 1):
+    ti, pi = map(int, (input().split()))
+    t[i], p[i] = ti, pi
+
 print(solve(N, t, p))
