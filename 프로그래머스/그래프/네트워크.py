ab1767d3e9197f6769c5313ec47102a2ea83bc08
SOHYEON <brightcattle@gmail.com>
2021-02-14 19:46:53 +0900

    Create 네트워크.py

---
+++ 프로그래머스/그래프/네트워크.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+from collections import deque
+
+def array_to_graph(computers, n):
+    graph = defaultdict(list)
+    for i in range(n):
+        for j in range(n):
+            if i != j and computers[i][j]:
+                graph[i+1].append(j+1)
+    return graph
+
+def bfs(graph, start, visited):
+    q = deque([start])
+    while q:
+        pos = q.popleft()
+        if visited[pos]:
+            continue
+        
+        visited[pos] = True
+        
+        for nextnode in graph[pos]:
+            q.append(nextnode)
+    
+def solution(n, computers):
+    answer = 0
+    graph = array_to_graph(computers, n)
+    visited = [False] * (n+1)
+    
+    for i in range(1, n+1):
+        if not visited[i]:
+            bfs(graph, i, visited)
+            answer += 1
+        
+    return answer
