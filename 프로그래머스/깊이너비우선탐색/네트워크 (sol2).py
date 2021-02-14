79f1766ace3c9f639a9d9c32fecab651e3da24e8
SOHYEON <brightcattle@gmail.com>
2021-02-14 21:41:38 +0900

    Update and rename 프로그래머스/그래프/네트워크 (sol2).py to 프로그래머스/깊이너비우선탐색/네트워크 (sol2).py

---
+++ 프로그래머스/깊이너비우선탐색/네트워크 (sol2).py
@@ -0,0 +1,19 @@
+def solution(n, computers):
+    answer = 0
+    visited = [0] * n
+
+    def dfs(n, computers, visited, start):
+        stack = [start]
+        while stack:
+            j = stack.pop()
+            if visited[j] == 0:
+                visited[j] = 1
+            for i in range(n):
+                if computers[j][i] == 1 and visited[i] == 0:
+                    stack.append(i)
+
+    for i in range(n):
+        if not visited[i]:
+            dfs(n, computers, visited, i)
+            answer += 1
+    return answer
