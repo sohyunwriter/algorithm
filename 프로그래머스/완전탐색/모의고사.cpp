9849b5ad2e43cd8f6bc1a6546f191f2945f561ea
SOHYEON <brightcattle@gmail.com>
2021-07-05 20:48:44 +0900

    Update and rename 모의고사.py to 모의고사.cpp

--- 프로그래머스/완전탐색/모의고사.cpp
+++ 프로그래머스/완전탐색/모의고사.cpp
@@ -1,8 +1,8 @@
+// 문제: https://programmers.co.kr/learn/courses/30/lessons/42840
+
 #include <string>
 #include <vector>
 #include <algorithm>
-
-// 문제: https://programmers.co.kr/learn/courses/30/lessons/42840
 
 using namespace std;
 
